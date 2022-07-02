7375aeae41a1a2b1ed65bb10c028b30e5c1f24df
Stefano Pontelli <stefanopontelli@hotmail.com>
2022-07-02 19:01:24 -0300

    finished challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Button } from "./Button";
 
 interface SideBarProps {
@@ -10,7 +11,7 @@ interface SideBarProps {
   buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({
+export function SideBarComponent({
   genres,
   selectedGenreId,
   buttonClickCallback
@@ -30,7 +31,14 @@ export function SideBar({
           />
         ))}
       </div>
-
+ 
     </nav>
   )
 }
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
+  return (
+    prevProps.selectedGenreId === nextProps.selectedGenreId &&
+    prevProps.genres.length === nextProps.genres.length
+  );
+});
